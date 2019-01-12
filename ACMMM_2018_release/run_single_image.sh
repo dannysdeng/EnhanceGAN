6c77099182409c52dd5cfc1fdaa46128800d168a
dannysdeng <danny.s.deng.ds@gmail.com>
2019-01-12 13:53:06 +0800

    Enable CPU-mode testing

--- ACMMM_2018_release/run_single_image.sh
+++ ACMMM_2018_release/run_single_image.sh
@@ -2,3 +2,7 @@ export CUDA_VISIBLE_DEVICES=0;
 FILENAME='1.jpg'
 th ./run_single.lua --filename $FILENAME --gpu 0;
 echo "Output saved to ./demo_result/"
+
+# This requires that copying the "checkpoints" folder (from Google Drive) to ./ACMMM_2018_release/
+# --gpu 1 to switch to GPU mode
+# --gpu 0 to switch to CPU mode
